f17849054b42e08a6db76d69ddf4ff34c78673f1
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2023-04-08 20:12:27 +0200

    More Prologish version of Snake

--- snake.pl
+++ snake.pl
@@ -1,5 +1,6 @@
 :- use_module(raylib).
 
+:- use_module(library(assoc)).
 :- use_module(library(lists)).
 :- use_module(library(dcgs)).
 :- use_module(library(time)).
@@ -7,12 +8,38 @@
 :- use_module(library(random)).
 :- use_module(library(clpz)).
 
-% State: Snake, Food, Time, Direction, LookAt, Ticks, End
+state_get([], _).
+state_get([Key-Value|Ps], S) :-
+    get_assoc(Key, S, Value),
+    state_get(Ps, S).
+state_get(Pairs), [S] -->
+    [S],
+    { state_get(Pairs, S) }.
+
+state_put(Ps), [S] -->
+    [S0],
+    { state_put_(Ps, S0, S) }.
+
+state_put_([], S, S).
+state_put_([Key-Value|Ps], S0, S) :-
+    put_assoc(Key, S0, Value, S1),
+    state_put_(Ps, S1, S).
+
 start :-
     init_window(400, 400, "Snake"),
     set_target_fps(60),
     get_time(Time),
-    loop(state([10-10, 10-11], [], Time, up, up, 0, false)).
+    list_to_assoc(
+	[
+	    snake-[10-10, 10-11],
+	    food-[],
+	    time-Time,
+	    direction-up,
+	    look_at-up,
+	    ticks-0,
+	    end-false
+	], State),
+    loop(State).
 
 loop(State0) :-
     (
@@ -21,7 +48,7 @@ loop(State0) :-
 	halt
     ;   (
 	inputs(Inputs),!,
-	update(Inputs, State0, State),!,
+	phrase(update(Inputs), [State0], [State]),!,
 	render(State),!,
 	loop(State)
         )
@@ -60,39 +87,42 @@ inputs_down_ -->
 	[down]
     ) | [].
 
-update(Inputs, State0, State) :-
-    \+ update_needed(State0, _),
-    State0 = state(Snake, Food, T, Dir0, LookAt, Ticks, false),
-    State = state(Snake, Food, T, Dir, LookAt, Ticks, false),
-    snake_dir(Inputs, Dir0, LookAt, Dir).
+update(Inputs) -->
+    state_get([time-Time, end-false]),
+    { \+ update_needed(Time, _) },
+    snake_dir(Inputs).
 
-update(Inputs, State0, State) :-
-    update_needed(State0, T),
-    State0 = state(Snake0, Food0, _, Dir0, LookAt0, Ticks0, false),
-    State = state(Snake, Food, T, Dir, Dir, Ticks, End),
-    Ticks is Ticks0 + 1,
-    snake_dir(Inputs, Dir0, LookAt0, Dir),
-    move_snake(Dir, Snake0, Snake1),
-    (
-	crash_snake(Snake1) ->
-	(
-	    End = true,
-	    Snake1 = Snake
-	)
-    ;   (
-	End = false,
-	food_update(Ticks, Snake1, Food0, Food1),
-	eat_update(Snake1, Snake, Food1, Food)
-        )
-    ).
+update(Inputs) -->
+    state_get([time-T0, ticks-Ticks0, end-false]),
+    state_put([time-T, ticks-Ticks, end-true]),
+    { update_needed(T0, T) },
+    { Ticks is Ticks0 + 1 },
+    snake_dir(Inputs),
+    move_snake,
+    crash_snake(yes).
 
-update(_Inputs, State, State) :-
-    State = state(_, _, _, _, _, _, true).
+update(Inputs) -->
+    state_get([time-T0, ticks-Ticks0, end-false]),
+    state_put([time-T, ticks-Ticks]),
+    { update_needed(T0, T) },
+    { Ticks is Ticks0 + 1 },
+    snake_dir(Inputs),
+    move_snake,
+    crash_snake(no),
+    food_update,
+    eat_update.
 
-update_needed(State0, Time1) :-
-    State0 = state(_, _, Time0, _, _, _, _),
+update(_Inputs) -->
+    state_get([end-true]).
+
+update_needed(Time0, Time1) :-
     get_time(Time1),
     Time1 > Time0 + 0.3 .
+
+snake_dir(Inputs) -->
+    state_get([direction-Dir0, look_at-LookAt]),
+    state_put([direction-Dir]),
+    { snake_dir(Inputs, Dir0, LookAt, Dir) }.
 
 snake_dir(Inputs, _, LookAt, left) :-
     member(left, Inputs),
@@ -114,10 +144,19 @@ snake_dir(Inputs, _, LookAt, down) :-
     \+ member(up, Inputs),
     LookAt \= up,!.
 
-snake_dir(_Inputs, Current, LookAt, Current).
+snake_dir(_Inputs, Current, _LookAt, Current).
 
-crash_snake([SnakeHead|SnakeTail]) :-
-    member(SnakeHead, SnakeTail).
+crash_snake(yes) -->
+    state_get([snake-[Head|Tail]]),
+    { member(Head, Tail) }.
+crash_snake(no) -->
+    state_get([snake-[Head|Tail]]),
+    { \+ member(Head, Tail) }.
+
+move_snake -->
+    state_get([snake-Snake0, direction-Dir]),
+    state_put([snake-Snake, look_at-Dir]),
+    { move_snake(Dir, Snake0, Snake) }.
 
 move_snake(left, Snake0, Snake) :-
     append([SnakeHead|SnakeTail], [_], Snake0),
@@ -142,6 +181,11 @@ move_snake(down, Snake0, Snake) :-
     SnakeHead = X-Y,
     NewY is (Y + 1) mod 20,
     Snake = [X-NewY,SnakeHead|SnakeTail].
+
+food_update -->
+    state_get([ticks-Ticks, snake-Snake, food-Food0]),
+    state_put([food-Food]),
+    { food_update(Ticks, Snake, Food0, Food) }.
 
 food_update(Ticks, Snake, Food0, Food) :-
     F is Ticks mod 15,
@@ -168,21 +212,26 @@ random_choice(Xs, X) :-
     random_integer(0, N, R),
     nth0(R, Xs, X).
 
+eat_update -->
+    state_get([snake-Snake0, food-Food0]),
+    state_put([snake-Snake, food-Food]),
+    { eat_update(Snake0, Snake, Food0, Food) }.
+
 eat_update(Snake0, Snake, Food0, Food) :-
     Snake0 = [SnakeHead|SnakeTail],
     Snake = [SnakeHead, SnakeHead|SnakeTail],
     select(SnakeHead, Food0, Food).
 
 eat_update(Snake0, Snake, Food0, Food) :-
-    Snake0 = [SnakeHead|SnakeTail],
+    Snake0 = [SnakeHead|_SnakeTail],
     Snake0 = Snake,
     Food0 = Food,
     \+ member(SnakeHead, Food0).
 
 render(State) :-
+    state_get([snake-Snake, food-Food, end-End], State),
     begin_drawing,
     clear_background(color(255, 255, 255, 255)),
-    State = state(Snake, Food, _, _, _, _, End),
     draw_snake(Snake),
     draw_food(Food),
     draw_score(Snake, End),
